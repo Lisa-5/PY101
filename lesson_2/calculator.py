9cc6d67c09b94aee09009801e88466d1a5c0e416
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-05-14 08:36:02 -0500

    formatting changes

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -12,16 +12,17 @@ number1 = input()
 print("What's the second number?")
 number2 = input()
 
-print('What opertion would you like to perform?\n1) Add  2) Subtract  3) Multiply  4) Divide')
+print('What opertion would you like to perform?\n'
+      '1) Add  2) Subtract  3) Multiply  4) Divide')
 operation = input()
 
-if operation == '1':
+if operation == '1':    # '1' represents addition
     output = int( number1) + int(number2)
-elif operation == '2':
+elif operation == '2':  # '2' represents subtraction
     output = int(number1) - int(number2)
-elif operation == '3':
+elif operation == '3':  # '3' represents multiplication
     output = int(number1) * int(number2)
-elif operation == '4':
+elif operation == '4':  # '4' represents divition
     output = int(number1) / int(number2)
 
 print(f'The result is: {output}')
