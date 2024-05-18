d9ad177db289218608e041f16a5901b800e61491
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-05-18 15:36:35 -0500

    allow floating numbers

--- lesson_2/calculator.py
+++ lesson_2/calculator.py
@@ -18,7 +18,7 @@ def prompt(message):
 
 def invalid_number(number_str):
     try:
-        int(number_str)
+        float(number_str)
     except ValueError:
         return True
 
@@ -48,13 +48,13 @@ def calculate():
 
     match operation:
         case '1':    # '1' represents addition
-            output = int( number1) + int(number2)
+            output = float( number1) + float(number2)
         case '2':  # '2' represents subtraction
-            output = int(number1) - int(number2)
+            output = float(number1) - float(number2)
         case '3':  # '3' represents multiplication
-            output = int(number1) * int(number2)
+            output = float(number1) * float(number2)
         case '4':  # '4' represents division
-            output = int(number1) / int(number2)
+            output = float(number1) / float(number2)
 
     prompt(data['result'].format(output))
 
