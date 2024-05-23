e0deb60ec26be407d1d7ec2750415f46debe34b3
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-05-23 08:56:26 -0500

    sum or product of consecutive integers

--- lesson_2/small_problems_exercises/easy_1/sum_prod_consec_ints.py
+++ lesson_2/small_problems_exercises/easy_1/sum_prod_consec_ints.py
@@ -1,14 +1,51 @@
-integer = int(input('Please enter an integer greater than 0: '))
-operation = input('Enter "s" to compute the sum, or "p" to computer the product. ')
-end = integer + 1
-product = 1
+def compute_sum(target_num):
+    return sum(range(1, target_num + 1))
 
-if operation == 's':
-    sum_total = sum(range(1, end))
-    print(f'The sum of the integers between 1 and {integer} is {sum_total}.')
-
-if operation == 'p':
-    for num in range(1, end):
+def compute_product(target_num):
+    product = 1
+    for num in range(1, target_num + 1):
         product *= num
-    print(f'The product of the integers between 1 and {integer} is {product}.')
-    
+    return product
+
+def invalid_number(num):
+    try:
+        int(num)
+    except ValueError:
+        return True
+    return False
+
+def validate_operation(string):
+    operation_type= ('s', 'p')
+    while string not in operation_type:
+        print()
+        print('Hmmm... that doesn\'t look like "s" or "p".\n'
+            'Please enter either "s" to compute a sum or "p" to compute a product. ')
+        string = input()
+
+def get_number():
+    print('Please enter an integer greater than 0: ')
+    num = input()
+
+    while invalid_number(num) or int(num) <= 0:
+        print()
+        print("Hmmm... that doesn't look like a valid integer greater than 0... try again: ")
+        num = input()
+
+    return int(num)
+
+def get_operation():
+    oper = input('Enter "s" to compute the sum, or "p" to compute the product. ')
+
+    validate_operation(oper)
+
+    return oper
+
+number = get_number()
+operation = get_operation()
+
+if operation == "s":
+    print("The sum of the integers between 1 and "
+          f"{number} is {compute_sum(number)}.")
+elif operation == "p":
+    print("The product of the integers between 1 and "
+          f"{number} is {compute_product(number)}.")
