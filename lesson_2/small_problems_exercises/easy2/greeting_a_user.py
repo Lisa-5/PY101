3e2755d8fec7c688dfc5e7ebde8b98a30172c680
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-05-24 13:03:25 -0500

    greeting a user exercise

---
+++ lesson_2/small_problems_exercises/easy2/greeting_a_user.py
@@ -0,0 +1,9 @@
+def greeting():
+    name = input('What is your name? ')
+    
+    if name.endswith('!'):
+       print(f"HELLO {name.upper()} WHY ARE WE YELLING?")
+    else:
+       print(f'Hello {name}.')
+
+greeting()
