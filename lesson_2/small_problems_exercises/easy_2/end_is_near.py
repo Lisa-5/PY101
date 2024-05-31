0973b134dc7e3d56eefadcb0323d9cce9a73dd7f
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-05-31 13:19:05 -0500

    update end is near to return middle word

--- lesson_2/small_problems_exercises/easy_2/end_is_near.py
+++ lesson_2/small_problems_exercises/easy_2/end_is_near.py
@@ -1,7 +1,21 @@
 def penultimate(string):
     words = string.split()
-    return words[-2]
+    length_of_words = len(words)
+
+    if length_of_words == 0:
+        return "empty string"
+    elif length_of_words == 1:
+        return words[0]
+    elif length_of_words % 2 == 1:
+        return words[(length_of_words // 2) + 1]
+    elif length_of_words % 2 == 0:
+        return 'no exact middle word'
 
 # These examples should print True
-print(penultimate("last word") == "last")
-print(penultimate("Launch School is great!") == "is")
+# print(penultimate("last word") == "last")
+# print(penultimate("Launch School is great!") == "is")
+
+print(penultimate(''))
+print(penultimate('last'))
+print(penultimate('last word test even number '))
+print(penultimate('last word test even number problem'))
