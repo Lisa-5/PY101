6a741dd22c8312b9ff195445fc8f96c998d3d24d
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-06-03 09:36:24 -0500

    finish easy_3

---
+++ lesson_3/easy_3.py
@@ -0,0 +1,38 @@
+# --- Question 1
+# numbers = [1, 2, 3, 4]
+# numbers.clear()
+# while numbers:
+#     numbers.pop()
+# numbers = []
+
+# print(numbers)
+
+# --- Question 2
+# print([1, 2, 3] + [4, 5])
+
+# --- Question 3
+# str1 = "hello there"
+# str2 = str1
+# str2 = "goodbye!"
+# print(str1)
+
+# --- Question 4
+# my_list1 = [{"first": "value1"}, {"second": "value2"}, 3, 4, 5]
+# my_list2 = my_list1.copy()
+# my_list2[0]['first'] = 42
+# print(my_list1)
+
+# --- Question 5
+# def is_color_valid(color):
+    # if color == "blue" or color == "green":
+    #     return True
+    # else:
+    #     return False
+    # return color in ('blue', 'green')
+    # return color == "blue" or color == "green"
+
+# print(is_color_valid('blue'))
+# print(is_color_valid('green'))
+# print(is_color_valid('red'))
+
+
