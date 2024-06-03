949e4d590c2d7615168b9517149d81c3c3c20da2
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-06-03 08:46:45 -0500

    finish easy_2

---
+++ lesson_3/easy_2.py
@@ -0,0 +1,57 @@
+# --- Question 1
+# numbers = [1, 2, 3, 4, 5]     # [5, 4, 3, 2, 1]
+# print(numbers[::-1])
+# print(numbers)
+# print()
+# print(list(reversed(numbers)))
+# print(numbers)
+
+# --- Question 2
+# numbers = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99]
+
+# number1 = 8  # False (not in numbers)
+# number2 = 95 # True (in numbers)
+
+# print(8 in numbers)
+# print(95 in numbers)
+
+# --- Question 3
+# print(42 >= 10 and 42 <= 100)
+# print(42 in range(10, 101))
+# print(100 in range(10, 101))
+# print(101 in range(10, 101))
+
+# --- Question 4
+# numbers = [1, 2, 3, 4, 5] 
+# numbers.pop(2)
+# del numbers[2]
+# print(numbers)
+
+# --- Question 5
+# numbers = [1, 2, 3, 4]
+# table = {'field1': 1, 'field2': 2, 'field3': 3, 'field4': 4}
+# print(type(numbers))
+# print(type(table))
+
+# --- Question 6
+# title = "Flintstone Family Members"
+# print(title.center(40))
+
+# --- Question 7
+# statement1 = "The Flintstones Rock!"
+# statement2 = "Easy come, easy go."
+
+# print(statement1.count('t'))
+# print(statement2.count('t'))
+
+# --- Question 8
+# ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 402, 'Eddie': 10}
+# print(ages.get('Spot'))
+# print('Spot' in ages)
+
+# --- Question 9
+# ages = {'Herman': 32, 'Lily': 30, 'Grandpa': 5843, 'Eddie': 10}
+# additional_ages = {'Marilyn': 22, 'Spot': 237}
+
+# ages.update(additional_ages)
+# print(ages)
