7c625bd9e2bcd8837933c5927c601b83924bcc65
Lisa <84411057+Lisa-5@users.noreply.github.com>
2024-09-16 15:23:56 -0500

    refactor daily double

--- lesson_2/small_problems_exercises/easy_3/ddaaiillyy_ddoouubbllee.py
+++ lesson_2/small_problems_exercises/easy_3/ddaaiillyy_ddoouubbllee.py
@@ -6,18 +6,15 @@ duplicate characters collapsed into a single character.
 
 def crunch(text):
     str_list = []
-    crunched = ''
 
     if text:
         str_list.append(text[0])
 
     for char in text:
-        if char == str_list[-1]:
-            continue
-        else:
+        if char != str_list[-1]:
             str_list.append(char)
 
-    return crunched.join(str_list)
+    return ''.join(str_list)
 
 # These examples should all print True
 print(crunch('ddaaiillyy ddoouubbllee') == 'daily double')
